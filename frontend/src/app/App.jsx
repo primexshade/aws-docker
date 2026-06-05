b85ed51791123f50b75e055ca3d9e11818613819
Aryan Tiwari <taryan54@gmail.com>
2026-06-06 03:24:07 +0530

    Monaco Editor Added

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css"
-
+import {Editor} from "@monaco-editor/react"
 
 
 function App() {
@@ -13,8 +13,13 @@ function App() {
         className="h-full w-1/4 bg-amber-50 rounded-lg"
       ></aside>
       <section
-      className="w-3/4 bg-neutral-800 rounded-lg">
-
+      className="w-3/4 bg-neutral-800 rounded-lg overflow-hidden">
+        <Editor
+        height="100%"
+        defaultLanguage="javascript"
+        defaultValue="// some comment"
+        theme="vs-dark"
+        />
       </section>
 
 
