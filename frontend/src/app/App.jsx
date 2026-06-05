38e82d08f4f3d0eaf3d27d489e1116f3ec0b0f6a
Aryan Tiwari <taryan54@gmail.com>
2026-06-06 03:14:51 +0530

    Base Layout

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,4 +1,4 @@
-
+import "./App.css"
 
 
 
@@ -6,7 +6,19 @@ function App() {
 
 
   return (
-    <></>
+    <main
+      className="h-screen w-full bg-gray-950 flex gap-4 p-4 "
+    >
+      <aside
+        className="h-full w-1/4 bg-amber-50 rounded-lg"
+      ></aside>
+      <section
+      className="w-3/4 bg-neutral-800 rounded-lg">
+
+      </section>
+
+
+    </main>
   )
 }
 
